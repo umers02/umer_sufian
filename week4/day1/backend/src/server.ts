cc9c866d887231aed9bb329bfd1e35aee5d75523
UmerSufian <umersufian02@gmail.com>
2025-12-15 15:01:37 +0500

    solve port issue

--- week4/day1/backend/src/server.ts
+++ week4/day1/backend/src/server.ts
@@ -12,10 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Connect to MongoDB
 connectDB();
 
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
@@ -72,11 +69,10 @@ import errorHandler from "./middlewares/errorHandler";
 app.use(errorHandler);
 
 // Start server
-if (process.env.NODE_ENV !== 'production') {
+
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
     console.log(`Swagger Docs: http://localhost:${PORT}/api-docs`);
   });
-}
 
 export default app;
